754fc445965c93db1558ceb41b9ccb3f4245c2e0
Morishita-mm <183205013+Morishita-mm@users.noreply.github.com>
2025-10-03 13:00:07 +0000

    add: Finish wcr project! but, no test passed because textbook's output format is different from  my env's one.

---
+++ wcr/src/lib.rs
@@ -0,0 +1,200 @@
+use clap::{Arg, ArgAction, Command};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: bool,
+    words: bool,
+    bytes: bool,
+    chars: bool,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    let mut total_lines = 0;
+    let mut total_words = 0;
+    let mut total_bytes = 0;
+    let mut total_chars = 0;
+    
+    for filename in &config.files {
+        match open(filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(file) => {
+                if let Ok(info) = count(file) {
+                    println!(
+                        "{}{}{}{}{}",
+                        format_field(info.num_lines, config.lines),
+                        format_field(info.num_words, config.words),
+                        format_field(info.num_bytes, config.bytes),
+                        format_field(info.num_chars, config.chars),
+                        if filename == "-" {
+                            "".to_string()
+                        } else {
+                            format!(" {}", filename)
+                        }
+                    );
+
+                    total_lines += info.num_lines;
+                    total_words += info.num_words;
+                    total_bytes += info.num_bytes;
+                    total_chars += info.num_chars;
+                }
+            }
+        }
+    }
+
+    if config.files.len() > 1 {
+        println!(
+            "{}{}{}{} total",
+            format_field(total_lines, config.lines),
+            format_field(total_words, config.words),
+            format_field(total_bytes, config.bytes),
+            format_field(total_chars, config.chars),
+        );
+    }
+
+    Ok(())
+}
+
+fn format_field(value: usize, show: bool) -> String {
+    if show {
+        format!("{:>8}", value)
+    } else {
+        "".to_string()
+    }
+}
+
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn count(file: impl BufRead) -> MyResult<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_bytes = 0;
+    let mut num_chars = 0;
+
+    for line in file.lines() {
+        let line = line?;
+
+        num_lines += 1;
+        num_bytes += line.len() + 1;
+        num_words += line.split_whitespace().count();
+        num_chars += line.chars().count();
+    }
+
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{FileInfo, count};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your halt.\r\n";
+        let info = count(Cursor::new(text));
+        assert!(info.is_ok());
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 10,
+            num_chars: 48,
+            num_bytes: 48,
+        };
+        assert_eq!(info.unwrap(), expected);
+    }
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("wcr")
+        .version("0.1.0")
+        .author("Mizuki <mizu.mizupica@gmail.com>")
+        .about("Rust wcr")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .default_value("-")
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .long("bytes")
+                .help("print the bytes counts")
+                .conflicts_with("chars")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("chars")
+                .short('m')
+                .long("chars")
+                .help("print the charactor counts")
+                .conflicts_with("words")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("lines")
+                .short('l')
+                .long("lines")
+                .value_name("VALUE")
+                .help("print the newline counts")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("words")
+                .short('w')
+                .long("words")
+                .help("print the words counts")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    let mut lines = matches.get_flag("lines");
+    let mut words = matches.get_flag("words");
+    let mut bytes = matches.get_flag("bytes");
+    let chars = matches.get_flag("chars");
+
+    // if !lines && !bytes && !(bytes || chars) {
+    //     (lines, words, bytes, chars) = (true, true, true, false);
+    // }
+
+    if [lines, words, bytes, chars].iter().all(|v| !v) {
+        lines = true;
+        words = true;
+        bytes = true;
+    }
+
+    Ok(Config {
+        files: matches
+            .get_many::<String>("files")
+            .unwrap()
+            .cloned()
+            .collect(),
+        lines: lines,
+        words: words,
+        bytes: bytes,
+        chars: chars,
+    })
+}

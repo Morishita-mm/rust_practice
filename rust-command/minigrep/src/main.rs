c82be425cbf43741ecc97bb1c1a7cefc8fd63bb2
Morishita-mm <183205013+Morishita-mm@users.noreply.github.com>
2025-10-06 00:31:58 +0000

    add: minigrep project finished!

---
+++ rust-command/minigrep/src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    // コマンドラインから引数を受け取る
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("APplocation error: {}", err);
+
+        process::exit(1);
+    }
+}
